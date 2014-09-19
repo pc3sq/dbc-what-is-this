6c9c5770538f2f608319c99d6bb0ddedbec3661e
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-19 18:59:25 -0400

    Add comment#index controller and view.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
