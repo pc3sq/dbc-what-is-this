ff4e9d0bf267055f60f50bf1b17749ee9a93cd80
n-zeplo <nzeplowitz@gmail.com>
2014-09-19 15:12:41 -0400

    Add Question Controller Methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,37 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @question = Question.all
+  end
+
+  def show
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.create(question_params)
+  end
+
+  def update
+    @question.update(post_params)
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+
+  def set_question
+      @question = Question.find(params[:id])
+  end
+
+  def post_params
+    params.require(:question).permit(:title,:caption,:image_path)
+  end
+
 end
