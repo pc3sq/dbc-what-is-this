f9cc6ccb1289ad1870e9078fa3adfea1fc9403c8
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-19 11:37:47 -0400

    Add comment, question, response, user, & vote model skeleton and full migrations.

---
+++ db/migrate/20140919151428_create_responses.rb
@@ -0,0 +1,11 @@
+class CreateResponses < ActiveRecord::Migration
+  def change
+    create_table :responses do |t|
+      t.text :content
+      t.references :user, index: true
+      t.references :question, index: true
+
+      t.timestamps
+    end
+  end
+end
