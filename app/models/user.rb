cca95727b9889cbc758ee9f11e87725165376c79
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-19 14:53:20 -0400

    Incomplete user validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   has_many :responses
   has_many :questions
 
+  validates :name, presence: true
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates_format_of :email, :with => (/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i)
+
+  validates :password, presence: true
+  # validates :password,
+
 end
