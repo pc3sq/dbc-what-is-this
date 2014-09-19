f8a8b1f0e9dbdb3ee60a47a288e154bcfc698053
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-19 16:47:01 -0400

    Add #create_slug and #before_save :create_slug.

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,13 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :responses
 
+  before_save :create_slug
+
   validates_format_of :image_path, :with => URI::regexp(%w(http https))
+
+  def create_slug
+    # self.title.gsub(' ', '-')
+    self.slug = self.title.parameterize
+  end
 
 end
