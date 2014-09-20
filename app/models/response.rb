a3f05cc4f072f1ab49443fb01f016497e2fd18e3
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-20 15:36:11 -0400

    Add dependent destroy to models

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,7 +1,7 @@
 class Response < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :votes
 
   validates :content, presence: true
