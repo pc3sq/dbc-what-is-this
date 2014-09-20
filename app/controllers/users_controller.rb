b553bbf679d4b4afb9c3533cef5053ae00357cbe
n-zeplo <nzeplowitz@gmail.com>
2014-09-20 17:25:32 -0400

    Add User Controller To Show User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :get_user, only: [:show, :update, :destroy]
 
   def show
-
+    @questions = Question.where(user: @user)
   end
 
   def new
@@ -16,13 +16,15 @@ class UsersController < ApplicationController
       session[:current_user] = @user.id
       redirect_to user_path(@user)
     else
-      flash[:notice] = "Bad email/password combination"
+      flash[:notice] = @user.errors.full_messages.to_sentence
       redirect_to signup_path
     end
   end
 
   def destroy
+    @user.destroy
 
+    redirect_to root
   end
 
   private
@@ -32,6 +34,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
