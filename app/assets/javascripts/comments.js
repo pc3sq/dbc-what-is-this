a387b68764716fd83f4a8d1c55cfa972365d1ce1
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-20 15:34:00 -0400

    Add comments js

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,3 +1,15 @@
+var commentForm =  '<form id="comment_form" action="/comments" method="POST" name="create_comment">
+    <textarea name="comment_form[body]" placeholder="Write your comment here." required></textarea>
+    <br>
+    <input type="submit" value="Add This Comment">
+  </form>'
+
+
+function appendCommentForm () {
+  $(".response").append(commentForm)
+}
+
+
 $(document).ready( function () {
   $("button.add_comment").click( function() {
     var responseId = $(".response").data("id");
@@ -7,7 +19,7 @@ $(document).ready( function () {
       method: 'POST',
       data: { id: $(this).data("id") }
     }).done( function (response) {
-      var selector = "span[data-id=" + targetId + "]";
+      var selector = "span[data-id=" + responseId + "]";
       $(selector).html(response);
       alert("Success!");
     }).fail( function (response) {
