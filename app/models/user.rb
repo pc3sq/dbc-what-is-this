f45fb39dace7bc9f60c9b71ead2df3ee0c73ba51
n-zeplo <nzeplowitz@gmail.com>
2014-09-20 17:30:06 -0400

    Fix Validations in User Model for Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ActiveRecord::Base
   has_many :responses
   has_many :questions
 
-  validates :name, presence: true
-  validates_format_of :name, :with => (/([a-zA-Z]|\d){2,}/) #refactor out into a CONSTANT?
-
   validates :email, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, :with => (/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i)
