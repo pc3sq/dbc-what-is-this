83a5a2eb545d8b9c6d9dd06e4966240a5be16129
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-20 15:41:27 -0400

    Rebase and fix merge conflicts

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,19 @@
+class CommentController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
