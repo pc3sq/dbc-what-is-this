b021fc7e10950353ecee6a7c33f11004be80707c
Deepak in the house.  & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-20 18:53:26 -0400

    Add validation so user can only vote once per response.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :response
+
+  validates :user, uniqueness: {  scope: :response,
+                                  message: "User can only vote on this response once."}
 end
