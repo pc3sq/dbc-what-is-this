b4fad35690b642ca0d8bebb7923811ea8e0c6863
n-zeplo <nzeplowitz@gmail.com>
2014-09-20 14:19:45 -0400

    Fix User Model Tests To Pass With Authentications

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20140919152929) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
