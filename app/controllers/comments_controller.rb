65f17580a6339f2bb81523c0342207500c5b88b4
Deepak in the house. & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-20 15:35:00 -0400

    Add comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,46 @@
 class CommentsController < ApplicationController
-  def index
-    @comments = Comment.all
-  end
 
-  def show
-  end
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def new
+    @comment = Comment.new
   end
 
-  def update
+  def create
+    response = Response.find(params[:response_id])
+    comment = Comment.new(comment_params)
+
+    if comment.save
+      response.comment << @comment
+      redirect_to @response.question
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def destroy
+  def update
+    @comment.update
+
+    redirect_to question_path(@question)
   end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to question_path(@question)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
 end
