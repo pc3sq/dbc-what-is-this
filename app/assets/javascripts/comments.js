b56f9c355d620c922f33759b844f7549e2e25b9a
Deepak in the house. & Peter Ching <pair+deepakswami07.pc3sq@devbootcamp.com>
2014-09-20 17:39:17 -0400

    Clean up comments js file.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,15 +1,3 @@
-var commentForm =  '<form id="comment_form" action="/comments" method="POST" name="create_comment">
-    <textarea name="comment_form[body]" placeholder="Write your comment here." required></textarea>
-    <br>
-    <input type="submit" value="Add This Comment">
-  </form>'
-
-
-function appendCommentForm () {
-  $(".response").append(commentForm)
-}
-
-
 $(document).ready( function () {
   $("button.add_comment").click( function() {
     var responseId = $(".response").data("id");
