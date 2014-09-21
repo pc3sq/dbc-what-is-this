2a104da8a3b964818475c6b0a61ebf8427b82758
Peter Ching <peter.ching@gmail.com>
2014-09-21 09:38:05 -0400

    Set up test skeleton for all public and private methods in comments controller.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -2,4 +2,34 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, :type => :controller do
 
+  describe 'comments controller' do
+
+    it '#new' do
+
+    end
+
+    it '#create' do
+
+    end
+
+    it '#update' do
+
+    end
+
+    it '#destroy' do
+
+    end
+
+    it 'private #comment_params' do
+
+      #To test a private method, you can use #send on a factory or instance of the comment class.
+
+    end
+
+    it 'private #set_comment' do 
+
+    end
+
+  end
+
 end
