2105b99ae909f6811af0dfedf8547933479dd313
deepakswami07 <deepakswami07@gmail.com>
2014-09-21 19:39:32 -0400

    Fix merge conflicts

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -10,50 +10,58 @@ RSpec.describe CommentsController, :type => :controller do
     @myresponse = Response.create(content:'hello world', question_id: @question.id, user_id: @user.id)
     @comment = Comment.create(body: "my comment", response_id: @myresponse.id, user_id: @user.id)
   end
-
+  
   describe "comment test success" do
 
   	it "comments#new" do
     	get :new, response_id: 1
    		expect(assigns(:comment)).to be_a Comment
   	end
-  end
 
     context "#create" do
     	it "creates a comment with valid params" do
         expect{
           post :create, response_id: @myresponse, comment: { body: "what is it", user_id: @user_id } 
           }.to change{Comment.count}.by(1)
+      end
+    end
+
+    context "comments#create" do
+
+    	it "creates a comment with valid params" do
+        post :create, {response_id: @myresponse, comment: {body: "what is it"} }
+        expect(response).to redirect_to("/questions/#{Comment.last.response.question.id}")
     	end
-  	end
 
       it "does not create a comment when params are invalid" do
       	post :create, response_id: @myresponse, comment: {body: nil, user_id: @user_id} 
         expect(response).to render_template(:new)
+      end
     end
 
-      context "#update" do
+    context "#update" do
 
-        it "it updates a comment when params are valid" do
-          put :update, id: @comment, comment: {body: "This is a great comment!"}
-          expect(response).to redirect_to("/questions/#{Comment.last.response.question.id}")
-        end
-
-        it "it does not update a comment when params are invalid" do
-          put :update, id: @comment, comment: {body: nil}
-          expect(response).to redirect_to("/questions/#{Comment.last.response.question.id}")
-        end
+      it "it updates a comment when params are valid" do
+        put :update, id: @comment, comment: {body: "This is a great comment!"}
+        expect(response).to redirect_to("/questions/#{Comment.last.response.question.id}")
       end
 
-      context "#destroy" do
-        it "should delete the comment" do
-        expect { delete :destroy, id: @comment }.to change(Comment,:count).by(-1)
+      it "it does not update a comment when params are invalid" do
+        put :update, id: @comment, comment: {body: nil}
+        expect(response).to redirect_to("/questions/#{Comment.last.response.question.id}")
       end
+
+    end
+
+    it "#destroy should delete the comment" do
+      expect { delete :destroy, id: @comment }.to change(Comment,:count).by(-1)
     end
 
     it "should redirect user to admin comments" do
-        delete :destroy, id: @comment
-        expect(response).to redirect_to("/questions/#{@myresponse.question.id}")
-      end
-end
+      delete :destroy, id: @comment
+      expect(response).to redirect_to("/questions/#{@myresponse.question.id}")
+    end
 
+  end
+
+end
