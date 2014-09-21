79216b9346ee7d5d11ae38167935464d46a0948f
n-zeplo <nzeplowitz@gmail.com>
2014-09-21 12:59:30 -0400

    Refactor Controllers In Response to Modals

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,15 @@ class UsersController < ApplicationController
 
   before_action :get_user, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @user = User.new
-  end
-
   def create
     @user = User.new(user_params)
 
     if @user.save
       session[:current_user] = @user.id
-      redirect_to root_path
     else
       flash[:notice] = @user.errors.full_messages.to_sentence
-      redirect_to signup_path
     end
+    redirect_to root_path
   end
 
   def show
