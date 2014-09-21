2a165c205bf988ce03d0872eb869509f7f9a638c
Peter Ching <peter.ching@gmail.com>
2014-09-21 13:37:17 -0400

    Change commentId to accurately be responseId.

--- app/assets/javascripts/responses.js
+++ app/assets/javascripts/responses.js
@@ -1,17 +1,17 @@
 $(document).ready( function () {
   $(".upvote").click( function() {
-    var commentId = $(".upvote").data("id");
+    var responseId = $(".upvote").data("id");
     event.preventDefault();
     $.ajax({
       url: '/response/up_vote',
       method: 'POST',
-      data: { id: commentId },
+      data: { id: responseId },
       dataType: 'JSON'
     }).done( function (voteCount) {
       if (voteCount == 1) {
-        $("span[data-id=" + commentId + "]").html(voteCount + " vote");
+        $("span[data-id=" + responseId + "]").html(voteCount + " vote");
       } else {
-        $("span[data-id=" + commentId + "]").html(voteCount + " vote");
+        $("span[data-id=" + responseId + "]").html(voteCount + " vote");
       }
     }).fail( function (voteCount) {
         console.log("Failed. Here is the voteCount:");
