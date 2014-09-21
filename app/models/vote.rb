84b5b2572993452950d65c5214dd9c9cb533903e
Peter Ching <peter.ching@gmail.com>
2014-09-21 07:54:04 -0400

    Add vote validations to ensure presence of a user id and a response id when creating a vote record.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,6 @@ class Vote < ActiveRecord::Base
 
   validates :user, uniqueness: {  scope: :response,
                                   message: "User can only vote on this response once."}
+                                  
+  validates_presence_of :user_id, :response_id
 end
