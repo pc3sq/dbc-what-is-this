83da23977780270ba94991714f826dd5ee73379c
Peter Ching <peter.ching@gmail.com>
2014-09-21 00:11:56 -0400

    Clean up comments and console.logs.

--- app/assets/javascripts/responses.js
+++ app/assets/javascripts/responses.js
@@ -2,18 +2,14 @@ $(document).ready( function () {
   $("button.upvote").click( function() {
     var commentId = $(".upvote").data("id");
     event.preventDefault();
-    // alert("Hi, I am data id: " + commentId)
     $.ajax({
       url: '/response/up_vote',
       method: 'POST',
-      data: { id: commentId }
+      data: { id: commentId },
+      dataType: 'JSON'
     }).done( function (response) {
-      // var selector = "span[data-id=" + responseId + "]";
-      // $(selector).html(response);
-      alert("Success! Here is your response: " + response);
     }).fail( function (response) {
         console.log(response);
-      // console.log("Failed");
     })
   })
 })
