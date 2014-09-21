240cef15c0b101a061b1b8997ddf4e3726714caf
deepakswami07 <deepakswami07@gmail.com>
2014-09-21 16:41:29 -0400

    incomplete controller test.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 
 RSpec.describe CommentsController, :type => :controller do
 
-  
   let(:comment){Comment.new(body: "This is a great comment!",
                         user_id: 2,
-                        response_id: 2)}
+                        response_id: 2)
+              }
 
-  let(:response){Comment.new(body: "This is a great response!",
-                        user_id: 1,
-                        response_id: 1)}
+  # let(:response){Response.create(content: "This is a great response!",
+  #                       user_id: 1,
+  #                       question_id: 1)
+  #             }
 
   describe "comment test success" do
 
@@ -21,10 +22,12 @@ RSpec.describe CommentsController, :type => :controller do
   	end
   end
 
+ 
     context "comments#create" do
     	it "creates a comment with valid params" do
-        post :create, {response_id: 1, comment: {body: "what is it"} }
-        expect(response).to redirect_to questions_path
+        post :create, comment: ({response_id: 1, user_id: 1, body: "what is it"} )
+
+        expect(response).to change{Comment.count}.by(1)
     	end
   	end
 
@@ -33,22 +36,30 @@ RSpec.describe CommentsController, :type => :controller do
         expect(response).to render_template(:new)
     end
 
-   #    context "#update" do
-   #      before(:each) do
+    # context "#create" do
+    #   it "creates a comment with valid params" do
+    #     expect {
+    #       post :create, :response_id { response_id: 1, comment: {body: "New comment!"} }
+    #       }.to change{Comment.count}.by(1)
+    #   end
+    # end
 
-   #        @comment = Comment.create({body: "This is a great comment!"})
-   #      end
+      # context "#update" do
+      #   before(:each) do
 
-   #      it "it updates a comment when params are valid" do
-   #        put :update, id: @comment, comment: {body: "This is a great comment!"}
-   #        expect(response).to redirect_to #ask peter
-   #      end
+      #     @comment = Comment.create({body: "This is a great comment!"})
+      #   end
 
-   #      it "it does not update a comment when params are invalid" do
-   #        put :update, id: @comment, comment: {body: nil}
-   #        expect(response).to render_template(:edit)
-   #      end
-   #    end
+      #   it "it updates a comment when params are valid" do
+      #     put :update, id: @comment, comment: {body: "This is a great comment!"}
+      #     expect(response).to redirect_to #ask peter
+      #   end
+
+      #   it "it does not update a comment when params are invalid" do
+      #     put :update, id: @comment, comment: {body: nil}
+      #     expect(response).to render_template(:edit)
+      #   end
+      # end
 
       # it "#destroy" do
       #   comment = Comment.new
