ca07ee10989c2d92f83a1ed7eaa85271147a4226
Peter Ching <peter.ching@gmail.com>
2014-09-21 10:53:25 -0400

    Resolved merge conflict by keeping the removal of code comments.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,1 +1,17 @@
-
+$(document).ready( function () {
+  $("button.add_comment").click( function() {
+    var responseId = $(".response").data("id");
+    event.preventDefault();
+    $.ajax({
+      url: '/comments/new',
+      method: 'POST',
+      data: { id: $(this).data("id") }
+    }).done( function (response) {
+      var selector = "span[data-id=" + responseId + "]";
+      $(selector).html(response);
+      alert("Success!");
+    }).fail( function (response) {
+      console.log("Failed");
+    })
+  })
+})
